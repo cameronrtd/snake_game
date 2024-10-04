d830859a278863b1a0bc7e70f36b5ff8cd7ce270
Cameron RT Dent <cameronrtd@gmail.com>
2024-10-04 21:23:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
+# create the controls of game
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -23,22 +24,23 @@ screen.onkey(snake.left, "Left")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.1) 
     snake.move()
 
+    
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.update_score()
         snake.extend()
 
-
-
+    
     # Detection with wall
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         game_is_on = False
         scoreboard.game_over()
 
+    
     # Detect collision with tail
     for n in range(3, scoreboard.score + 2):
         if snake.head.distance(snake.segments[n]) < 10:
