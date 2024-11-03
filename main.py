c8c43e5a1e16ca420831e056e82f90adfb704823
Cameron RT Dent <cameronrtd@gmail.com>
2024-11-03 11:24:27 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from turtle import Screen
 from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
+
+# Set up the screen
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
+# create objects
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
